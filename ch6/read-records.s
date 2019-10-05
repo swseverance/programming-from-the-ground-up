aa9c1a254965b20a45808f5c43c2c661b8b097f2
Sam Severance <swseverance@gmail.com>
2019-10-05 05:57:39 -0400

    ch6

--- ch6/read-records.s
+++ ch6/read-records.s
@@ -1,3 +1,5 @@
+# as -g -o build/read-record.o ch6/read-record.s && as -g -o build/read-records.o ch6/read-records.s as -g -o build/count-chars.o ch6/count-chars.s && ld -o build/read-records build/read-records.o build/read-record.o build/count-chars.o && ./build/read-records
+
 .include "ch6/linux.s"
 .include "ch6/record-def.s"
 
@@ -26,6 +28,30 @@ _start:
     mov $0, %rsi
     mov $0666, %rdx
     syscall
+
+    push %rax       # push input file descriptor onto stack
+    push $STDOUT    # push output file descriptor onto stack
+
+record_read_loop:
+    # invoke read_record function
+    push INPUT_DESCRIPTOR(%rbp)
+    push $RECORD_BUFFER
+    call read_record
+    add $16, %rsp       # pop function args off of stack
+
+    cmp $RECORD_SIZE, %rax
+    jne finished_reading
+
+    push $RECORD_FIRSTNAME + RECORD_BUFFER
+    call count_chars
+    add $8, %rsp        
+
+    mov %rax, %rdx                      # count of chars to print
+    mov $RECORD_BUFFER, %rsi
+    mov OUTPUT_DESCRIPTOR(%rbp), %rdi
+    mov $SYS_WRITE, %rax
+    syscall
+    jmp record_read_loop
 
 finished_reading:
     mov $SYS_EXIT, %rax
